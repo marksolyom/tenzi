2ba7dff883cfea6e342e2b90fe97468533e17245
marksolyom <solyom.mark@gmail.com>
2022-08-17 16:20:56 +0200

    add dice state and render with button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+import {useState} from 'react';
 import Die from "./components/Die"
 
 export default function App() {
+
+  const [dice, setDice] = useState(newDice())
+
+  function newDice() {
+    const diceArr = [];
+    for(let i=0; i<10; i++) {
+      diceArr.push(Math.floor(Math.random()*6+1));
+    }
+    return diceArr;
+  }
+
+  function rollDice() {
+    setDice(newDice())
+  }
+
   return (
     <div className="App">
       <main>
-        <div className="die-container">
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
-          <Die value="2"/>
+        <div className="dice-container">
+          {dice.map(die => <Die value={die} />)}
         </div>
+        <button className="roll-btn" onClick={rollDice}>Roll</button>
       </main>
     </div>
   );
