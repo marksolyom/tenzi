97cc1ecb955fa0ea2c1b0010d08ebbd50d31f95b
marksolyom <solyom.mark@gmail.com>
2022-08-17 14:24:14 +0200

    create base component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Die from "./components/Die"
+
 export default function App() {
   return (
     <div className="App">
-      
+      <main>
+        <div className="die-container">
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+          <Die value="2"/>
+        </div>
+      </main>
     </div>
   );
 }
