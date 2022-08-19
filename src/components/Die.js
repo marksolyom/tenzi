3370be8e4936fb6636c5d28083a0535673856301
marksolyom <solyom.mark@gmail.com>
2022-08-19 12:59:52 +0200

    replace numbers with dots

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,11 +1,16 @@
 export default function Die(props) {
     return (
         <div
-            className="die"
+            className={`die face${props.value}`}
             style={{ backgroundColor: props.isHeld ? '#59E391' : 'white' }}
             onClick={props.holdDice}
         >
-            {props.value}
+            {props.value > 0 && <span className="dot" />}
+            {props.value > 1 && <span className="dot" />}
+            {props.value > 2 && <span className="dot" />}
+            {props.value > 3 && <span className="dot" />}
+            {props.value > 4 && <span className="dot" />}
+            {props.value > 5 && <span className="dot" />}
         </div>
     )
 }
